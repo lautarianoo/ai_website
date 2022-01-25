727d08b44f2bb8830b42cbd8407add7e051d9c40
lautariano <lautariano777@gmail.com>
2022-01-25 17:53:35 +0500

    fix setting

--- techno_websiteMy/settings.py
+++ techno_websiteMy/settings.py
@@ -129,8 +129,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 
 
