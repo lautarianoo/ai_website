e82778e1da2da945c2b2a10bc6b61fae17f7e763
lautariano <lautariano777@gmail.com>
2022-01-25 17:07:16 +0500

    Diploy

--- techno_websiteMy/settings.py
+++ techno_websiteMy/settings.py
@@ -11,22 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-56_s%^(d^ek&mzdzn-h(+%uc!h!wyxo5!(#4d47%u+w1y+vgsb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -89,6 +87,8 @@ DATABASES = {
         'PORT': '5432'
     }
 }
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
