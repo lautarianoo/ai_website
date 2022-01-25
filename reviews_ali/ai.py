5b47692165983e505d00426d1890294d106942c4
lautariano <lautariano777@gmail.com>
2022-01-25 20:09:53 +0500

    epochs

--- reviews_ali/ai.py
+++ reviews_ali/ai.py
@@ -64,7 +64,7 @@ def neuroview(index):
     )
 
     '''Обучение модели'''
-    model.fit(train_images, train_labels, epochs=3)
+    model.fit(train_images, train_labels, epochs=2)
     test_loss, test_acc = model.evaluate(test_images, test_labels)  # Оценка точности
     data = {
         'test_accuracy': int(test_acc * 100)
